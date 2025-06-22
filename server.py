8f363641983a771a661e17efe31c61cd5a101c27
Mizuki Hikaru <mizuki@hikaru.org>
2025-06-22 02:45:55 +0000

    add server, some templates

---
+++ server.py
@@ -0,0 +1,47 @@
+from uvicorn import run
+from fastapi import FastAPI, UploadFile, File, HTTPException, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+from yamldoc import yamldoc
+import shutil
+import tempfile
+import os
+
+app = FastAPI()
+
+@app.post("/yaml2resume")
+async def resume_renderer(file: UploadFile = File(...), background_tasks: BackgroundTasks = None):
+    template_path = Path.cwd()
+
+    try:
+        temp_dir = tempfile.mkdtemp()
+        temp_path = Path(temp_dir)
+
+        uploaded_file_path = temp_path / "input.yaml"
+        with uploaded_file_path.open("wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        output_pdf_path = temp_path / "output.pdf"
+
+        yamldoc(uploaded_file_path, output_pdf_path)
+
+        # Schedule the temp dir to be deleted after response is sent
+        background_tasks.add_task(shutil.rmtree, temp_path)
+
+        # Send response
+        return FileResponse(
+            path=output_pdf_path,
+            media_type="application/pdf",
+            filename="Resume.pdf",
+            background=background_tasks
+        )
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=500, detail=str(e))
+
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
+
+if __name__ == "__main__":
+    run("server:app", host="0.0.0.0", port=8000, reload=True)
